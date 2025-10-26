6b8d7553e04ec33d1f8a0f44b56d7c1d9a6f252d
Esther Manrique <esthermanriqueepam@gmail.com>
2025-10-25 19:50:57 -0600

    Implement routing and enhance UI components with forms and menus

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () =>
+      import('./reactive/reactive.routs').then((m) => m.reactiveRoutes),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routs'),
+  },
+  {
+    path: 'country',
+    loadChildren: () =>
+      import('./country/country.routs').then((m) => m.countryRoutes),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive',
+  }
+];
