ca5742660a7281dd688501685fbdb4bfcb47c54a
Esther Manrique <esthermanriqueepam@gmail.com>
2025-10-26 15:05:49 -0600

    Add FormUtils utility class for form validation and error handling

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,48 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
-  styleUrl: './basic-page.component.css'
+  styleUrl: './basic-page.component.css',
 })
 export class BasicPageComponent {
+  // Using FormGroup and FormControl
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
 
+  // Using FormBuilder Better Approach
+  private fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  onSave() {
+    console.log({'Guardando...': this.myForm.value});
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0,
+    });
+  }
 }
